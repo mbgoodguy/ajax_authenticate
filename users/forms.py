63085ea20d79b0cbba6b1bb7551bdc557a7fb0bb
mb <nerooren4001@gmail.com>
2023-12-03 20:53:31 +0300

    Add templates

---
+++ users/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import (
+    UserCreationForm as UserCreationFormDjango
+)
+from django import forms
+from django.utils.translation import gettext_lazy
+
+User = get_user_model()
+
+
+class UserCreationForm(UserCreationFormDjango):
+    email = forms.EmailField(
+        label=gettext_lazy("Email"),
+        max_length=254,
+        widget=forms.EmailInput(attrs={'autocomplete': 'email'})
+    )
+
+    class Meta(UserCreationFormDjango.Meta):
+        model = User
+        fields = ("username", "email")
